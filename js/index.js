4ccf1d99d79d15f46d63c1d936c1a9d911b62fa7
ritaurbani <ritaurbani@gmail.com>
2024-10-30 09:48:24 +0100

    feat-correct flow

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 
 
-for ( let i=1; i<=100 ; i++) {
 
-    if ((i % 3 === 0) && (i % 5 === 0) ) {
-        console.log('Fizz')
-    } else if (i % 5 === 0) {
-        console.log('Buzz')
+
+for (let i = 1; i <= 100; i++) {
+
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.error('FizzBuzz');
     } else if (i % 3 === 0) {
-        console.log('FizzBuzz')
+        console.warn('Fizz');
+    } else if (i % 5 === 0) {
+        console.warn('Buzz');
     } else {
-        console.log(i)
+        console.log(i);
     }
-
 }
+
+------------------------------------------
+
